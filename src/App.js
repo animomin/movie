46662eab012df48a2dbac38a2223cd5e2bd28abc
jaemin_seo <sjm@neochart.co.kr>
2020-01-14 10:49:51 +0900

    rendering the Movies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
 // react는 자동적으로 class component의 render method를 실행한다.
 class App extends React.Component {
@@ -11,7 +12,15 @@ class App extends React.Component {
   // async : 약간의 시간이 걸릴 수 있어!
   getMovies = async () => {
     //             axios를 기다려야해!
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+
+    this.setState({ movies, isLoading: false });
   };
 
   componentDidMount() {
@@ -20,8 +29,26 @@ class App extends React.Component {
   }
 
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading" : "We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading"
+          : movies.map(movie => {
+              console.log(movie);
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              );
+            })}
+      </div>
+    );
   }
 }
 
