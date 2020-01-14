ac3e693543fcfbbf3c99063f7ee7d3e644e39256
jaemin_seo <sjm@neochart.co.kr>
2020-01-14 09:45:19 +0900

    componentDidMount

--- src/App.js
+++ src/App.js
@@ -3,25 +3,22 @@ import React from "react";
 // react는 자동적으로 class component의 render method를 실행한다.
 class App extends React.Component {
   state = {
-    count: 0
+    isLoading: true,
+    movies: []
   };
 
-  add = () => {
-    this.setState(current => ({ count: ++current.count }));
-  };
-
-  minus = () => {
-    this.setState(current => ({ count: --current.count }));
-  };
+  componentDidMount() {
+    // data fetching is here
+    setTimeout(() => {
+      this.setState({
+        isLoading: false
+      });
+    }, 6000);
+  }
 
   render() {
-    return (
-      <div>
-        <h1>The number is {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading" : "We are ready"}</div>;
   }
 }
 
