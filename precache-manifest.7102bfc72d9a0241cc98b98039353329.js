4f85bb8ca9a4b4f7f7b32bdbf6687fc6a832a973
jaemin_seo <sjm@neochart.co.kr>
2020-01-14 12:19:30 +0900

    Updates

---
+++ precache-manifest.7102bfc72d9a0241cc98b98039353329.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43995b0092ca41b11f5ae744a4fd0aaa",
+    "url": "//animomin.github.io/movie/index.html"
+  },
+  {
+    "revision": "e69feebecd6e7c4263cb",
+    "url": "//animomin.github.io/movie/static/css/main.10b0ebdd.chunk.css"
+  },
+  {
+    "revision": "c67447c5b3a9f73484aa",
+    "url": "//animomin.github.io/movie/static/js/2.382d1d7a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "//animomin.github.io/movie/static/js/2.382d1d7a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e69feebecd6e7c4263cb",
+    "url": "//animomin.github.io/movie/static/js/main.b9bd7d39.chunk.js"
+  },
+  {
+    "revision": "db02a3dd57c0dae1d154",
+    "url": "//animomin.github.io/movie/static/js/runtime-main.182f2508.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "//animomin.github.io/movie/precache-manifest.7102bfc72d9a0241cc98b98039353329.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//animomin.github.io/movie/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
