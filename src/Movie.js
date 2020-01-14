476d4deb17cd378d571295a2212183fb6693ecc1
jaemin_seo <sjm@neochart.co.kr>
2020-01-14 11:35:03 +0900

    adding genres

--- src/Movie.js
+++ src/Movie.js
@@ -2,7 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({ year, title, summary, poster }) {
+function Movie({ year, title, summary, poster, genres }) {
+  console.log(genres);
   return (
     <div className="movie">
       <img src={poster} alt={title} title={title} />
@@ -11,7 +12,8 @@ function Movie({ year, title, summary, poster }) {
           {title}
         </h3>
         <h5 className="movie__year">{year}</h5>
-        <p className="moovie__summary">{summary}</p>
+        <span className="movie__genre">{genres.join(",")}</span>
+        <p className="movie__summary">{summary}</p>
       </div>
     </div>
   );
@@ -22,7 +24,8 @@ Movie.propTypes = {
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
